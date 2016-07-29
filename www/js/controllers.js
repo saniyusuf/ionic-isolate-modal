adf5cd7382c311cac54c585a75f4f8c17402e18e
Sani Yusuf <saniyusuf@gmail.com>
2016-07-29 17:46:30 +0100

    pushed

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,28 +1,39 @@
-angular.module('starter.controllers', [])
+angular
+  .module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+  .controller('DashCtrl', function($scope, IsolateModal) {
+    $scope.openModal = function () {
+      IsolateModal.open();
+    };
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+  })
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
+  .controller('ChatsCtrl', function($scope, Chats, IsolateModal) {
+    $scope.openModal = function () {
+      IsolateModal.open();
+    };
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+    $scope.chats = Chats.all();
+    $scope.remove = function(chat) {
+      Chats.remove(chat);
+    };
+  })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+  .controller('ChatDetailCtrl', function($scope, $stateParams, Chats, IsolateModal) {
+    $scope.openModal = function () {
+      IsolateModal.open();
+    };
+
+    $scope.chat = Chats.get($stateParams.chatId);
+  })
+
+  .controller('AccountCtrl', function($scope, IsolateModal) {
+
+    $scope.openModal = function () {
+      IsolateModal.open();
+    };
+
+    $scope.settings = {
+      enableFriends: true
+    };
+  });
--- www/js/services.js
+++ www/js/services.js
@@ -1,50 +1,93 @@
-angular.module('starter.services', [])
+(function () {
+  'use strict';
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
+  angular
+    .module('starter.services', [])
 
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
-  }];
+    .factory('IsolateModal', IsolateModal)
 
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+    .factory('Chats', function() {
+      // Might use a resource here that returns a JSON array
+
+      // Some fake testing data
+      var chats = [{
+        id: 0,
+        name: 'Ben Sparrow',
+        lastText: 'You on your way?',
+        face: 'img/ben.png'
+      }, {
+        id: 1,
+        name: 'Max Lynx',
+        lastText: 'Hey, it\'s me',
+        face: 'img/max.png'
+      }, {
+        id: 2,
+        name: 'Adam Bradleyson',
+        lastText: 'I should buy a boat',
+        face: 'img/adam.jpg'
+      }, {
+        id: 3,
+        name: 'Perry Governor',
+        lastText: 'Look at my mukluks!',
+        face: 'img/perry.png'
+      }, {
+        id: 4,
+        name: 'Mike Harrington',
+        lastText: 'This is wicked good ice cream.',
+        face: 'img/mike.png'
+      }];
+
+      return {
+        all: function() {
+          return chats;
+        },
+        remove: function(chat) {
+          chats.splice(chats.indexOf(chat), 1);
+        },
+        get: function(chatId) {
+          for (var i = 0; i < chats.length; i++) {
+            if (chats[i].id === parseInt(chatId)) {
+              return chats[i];
+            }
+          }
+          return null;
         }
-      }
-      return null;
+      };
+    });
+
+  IsolateModal.$inject = ['$rootScope', '$ionicModal'];
+  function IsolateModal($rootScope, $ionicModal) {
+    var modalsIsolateScope = $rootScope.$new();
+    var isolateModal = {
+      open: open
+    };
+
+    function open() {
+      $ionicModal.fromTemplateUrl(
+        'templates/isolate-modal.html',
+        {
+          scope: modalsIsolateScope
+        }
+      )
+        .then(function (modalInstance) {
+          modalsIsolateScope.close = function () {
+            closeAndRemove(modalInstance);
+          };
+          return modalInstance.show();
+      });
+
     }
-  };
-});
+
+    function closeAndRemove(modalInstance) {
+      return modalInstance.hide()
+        .then(function () {
+          return modalInstance.remove();
+        });
+    }
+
+    return isolateModal;
+  }
+
+
+})();
+
